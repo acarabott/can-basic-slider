76dba76e1a9d1e6a120d8f48d8843bef1f0d2d23
Arthur Carabott <arthurc@gmail.com>
2017-02-28 07:55:41 +0000

    Initial Commit

---
+++ can-basic-slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  can-basic-slider
+//
+//  Created by Arthur Carabott on 28/02/2017.
+//  Copyright © 2017 Arthur Carabott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
