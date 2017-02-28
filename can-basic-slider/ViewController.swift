a4c74bbb5f8a42ec5a3902edb9bd4232b965341d
Arthur Carabott <arthurc@gmail.com>
2017-02-28 09:31:55 +0000

    min and max working properly

--- can-basic-slider/ViewController.swift
+++ can-basic-slider/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
   // MARK: Properties
   @IBOutlet weak var slider: UISlider!
@@ -17,7 +17,10 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    min.delegate = self;
+    max.delegate = self;
+    min.text = "0.0";
+    max.text = "1.0";
   }
 
   override func didReceiveMemoryWarning() {
@@ -25,10 +28,24 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  // MARK: UITextFieldDelegate
+
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // hide the keyboard
+    textField.resignFirstResponder();
+    return true;
+  }
+
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    print("did end editing!");
+  }
+
   // MARK: Actions
 
   @IBAction func sliderAction(_ sender: UISlider, forEvent event: UIEvent) {
     print(sender.value);
+    min.resignFirstResponder();
+    max.resignFirstResponder();
   }
 
   @IBAction func minAction(_ sender: UITextField) {
@@ -38,6 +55,11 @@ class ViewController: UIViewController {
   @IBAction func maxAction(_ sender: UITextField) {
     slider.maximumValue = Float(sender.text!) ?? 1.0;
   }
+
+  @IBAction func minChanged(_ sender: UITextField) {
+    print("min changed");
+  }
+
 
 }
 
